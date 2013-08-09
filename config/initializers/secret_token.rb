ead2858e6f2325b1c43ad432a481b9eb8e159c5e
Brant <brant@pegasus.(none)>
2013-08-09 14:16:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brant::Application.config.secret_key_base = '9906f9582fd43d8ea1456ff0f34a00657bca7e1e3733b781a0194133d2bdb64ba27fd6773e0584bcdf1e9b82622a9259a1c06019bdf09ce20a0a5b8318de30d2'
